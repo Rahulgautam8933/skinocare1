f30a4212c470e89228201b6fcbd1671d5155f29e
Rahul <rahul>
2023-10-10 18:58:47 +0530

    imagexomntent

--- src/componets/imgcontent/Imgcontent.js
+++ src/componets/imgcontent/Imgcontent.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 
 import Carousel from 'react-bootstrap/Carousel';
 
 import Button from '../button/Button'
 function Imgcontent({ secimg, h1, p, secimg2, secimg3, secimg4, secimg5 }) {
+
+
+ 
 
 
   return (
@@ -48,8 +51,6 @@ function Imgcontent({ secimg, h1, p, secimg2, secimg3, secimg4, secimg5 }) {
                     </Carousel.Caption>
                   </Carousel.Item>
                 </Carousel>
-
-              
               </div>
             </div>
           </div >
--- src/pages/SignupSignin/ForgetPassword.js
+++ src/pages/SignupSignin/ForgetPassword.js
@@ -58,6 +58,8 @@ const ForgetPassword = () => {
                         
                     }
 
+                    
+
                     </div>
                 </div>
             </div>
